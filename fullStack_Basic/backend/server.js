5045d0bc106bee08a339ead4f5065e812d904650
iamanu26 <anuragdubey26202@gmail.com>
2025-11-30 22:43:48 +0530

    Backend learning

---
+++ fullStack_Basic/backend/server.js
@@ -0,0 +1,46 @@
+import express from 'express';
+
+const app = express();
+
+// app.get('/' , (req , res) => {
+//     res.send('Server is ready');
+// });
+
+// get a list of five jokes
+
+app.get('/api/jokes' , (req , res) => {
+    const jokes  = [
+        {
+            id:1,
+            title: 'A joke',
+            content: 'This is a joke'
+        },
+        {
+            id:2,
+            title: 'Another joke',
+            content: 'This is a joke'
+        },
+        {
+            id:3,
+            title: 'A Third joke',
+            content: 'This is third joke'
+        },
+        {
+            id:4,
+            title: 'Fourth joke',
+            content: 'This is fourth joke'
+        },
+        {
+            id:5,
+            title: 'Fifth joke',
+            content: 'This is fifth joke'
+        }
+    ];
+    res.send(jokes);
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port , ()=> {
+    console.log('Server is ready');
+});
